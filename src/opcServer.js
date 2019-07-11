7b505ca6cdcf6be58426c96177885ca94f7e3267
Guilherme Francescon <guilherme.francescon@netsmarttech.com>
2019-07-11 19:24:26 -0300

    Implements OPCServer methods

--- src/opcServer.js
+++ src/opcServer.js
@@ -5,18 +5,45 @@
 */
 
 const constants = require('./constants.js');
+const EnumString = require('./enumString.js');
 const OPCCommon = require('./opcCommon.js');
 const OPCBrowser = require('./opcBrowser.js');
 const OPCItemIO = require('./opcItemIO.js');
 const OPCItemProperties = require('./opcItemProperties');
+const OPCGroupStateManager = require('./opcGroupStateManager');
+const filetime = require('./filetime');
+
+const dcom = require('dcom');
+
+const groupCache = new WeakMap();
 
 /**
  * Represents an OPC Server
  */
 class OPCServer {
 
-    constructor() {
+    /**
+     * 
+     * @param {object} [opts]
+     * @param {object} [opts.groupDefaults]
+     * @param {boolean} [opts.groupDefaults.active=true]
+     * @param {number} [opts.groupDefaults.updateRate=1000]
+     * @param {number} [opts.groupDefaults.timeBias=60]
+     * @param {number} [opts.groupDefaults.deadband=0]
+     * @param {number} [opts.defaultLocale=1033]
+     */
+    constructor(opts) {
         //TODO - default values for addGroup
+        opts = opts || {};
+        this._defaultLocale = opts.defaultLocale || 1033;
+
+        let def = opts.groupDefaults || {};
+        this._groupDef = {
+            active: !!(def.active !== undefined ? def.active : true),
+            updateRate: !isNaN(def.updateRate) ? def.updateRate : 1000,
+            timeBias: !isNaN(def.timeBias) ? def.timeBias : 60,
+            deadband: !isNaN(def.deadband) ? def.deadband : 0,
+        }
 
         this._comObj = null;
         this._opcCommon = null;
@@ -74,6 +101,34 @@ class OPCServer {
     async addGroup(name, opts) {
         if (!this._comObj) throw new Error("Not initialized");
 
+        let active = opts.active !== undefined ? opts.active : this._groupDef.active;
+        let updateRate = !isNaN(opts.updateRate) ? opts.updateRate : this._groupDef.updateRate;
+        let clientHandle = !isNaN(opts.clientHandle) ? opts.clientHandle : Math.trunc(Math.random() * 0xFFFFFFFF);
+        let timeBias = !isNaN(opts.timeBias) ? opts.timeBias : this._groupDef.timeBias;
+        let deadband = !isNaN(opts.deadband) ? opts.deadband : this._groupDef.deadband;
+        let localeID = !isNaN(opts.localeID) ? opts.localeID : this._defaultLocale;
+
+        let callObject = new dcom.CallBuilder(true);
+        callObject.setOpnum(0);
+
+        callObject.addInParamAsString(name, dcom.Flags.FLAG_REPRESENTATION_STRING_LPWSTR);
+        callObject.addInParamAsInt(active ? 1 : 0, dcom.Flags.FLAG_NULL);
+        callObject.addInParamAsInt(updateRate, dcom.Flags.FLAG_NULL);
+        callObject.addInParamAsInt(clientHandle, dcom.Flags.FLAG_NULL);
+        callObject.addInParamAsPointer(new dcom.Pointer(timeBias), dcom.Flags.FLAG_NULL);
+        callObject.addInParamAsPointer(new dcom.Pointer(deadband), dcom.Flags.FLAG_NULL);
+        callObject.addInParamAsInt(localeID, dcom.Flags.FLAG_NULL);
+        callObject.addOutParamAsType("Integer", dcom.Flags.FLAG_NULL);
+        callObject.addOutParamAsType("Integer", dcom.Flags.FLAG_NULL);
+        callObject.addInParamAsUUID(constants.iid.IOPCGroupStateMgt_IID, dcom.Flags.FLAG_NULL);
+        callObject.addOutParamAsType("ComObject", dcom.Flags.FLAG_NULL);
+
+        let result = await this._comObj.call(callObject);
+
+        let group = new OPCGroupStateManager();
+        await group.init(result[2]);
+        groupCache.set(group, clientHandle);
+        return group;
     }
 
     /**
@@ -86,6 +141,20 @@ class OPCServer {
     async getErrorString(error, localeID) {
         if (!this._comObj) throw new Error("Not initialized");
 
+        let callObject = new dcom.CallBuilder(true);
+        callObject.setOpnum(1);
+
+        localeID = localeID || this._defaultLocale;
+
+        callObject.addInParamAsInt(error, dcom.Flags.FLAG_NULL);
+        callObject.addInParamAsInt(localeID, dcom.Flags.FLAG_NULL);
+        callObject.addOutParamAsObject(new dcom.ComString(dcom.Flags.FLAG_REPRESENTATION_STRING_LPWSTR), dcom.Flags.FLAG_NULL);
+        //callObject.addOutParamAsObject(new dcom.Pointer(new dcom.ComString(dcom.Flags.FLAG_REPRESENTATION_STRING_LPWSTR)), dcom.Flags.FLAG_NULL);
+
+        let result = await this._comObj.call(callObject);
+
+        return result[0].getString();
+        //return result[0].getReferent().getString();
     }
 
     /**
@@ -97,6 +166,18 @@ class OPCServer {
     async getGroupByName(name) {
         if (!this._comObj) throw new Error("Not initialized");
 
+        let callObject = new dcom.CallBuilder(true);
+        callObject.setOpnum(2);
+
+        callObject.addInParamAsString(name, dcom.Flags.FLAG_REPRESENTATION_STRING_LPWSTR);
+        callObject.addInParamAsUUID(constants.iid.IOPCGroupStateMgt_IID, dcom.Flags.FLAG_NULL);
+        callObject.addOutParamAsType("ComObject", dcom.Flags.FLAG_NULL);
+
+        let result = await this._comObj.call(callObject);
+
+        let group = new OPCGroupStateManager();
+        await group.init(result[2]);
+        return group;
     }
 
     /**
@@ -106,11 +187,47 @@ class OPCServer {
     async getStatus() {
         if (!this._comObj) throw new Error("Not initialized");
 
+        let statusStruct = new dcom.Struct();
+        statusStruct.addMember(filetime.getStruct()); //startTime
+        statusStruct.addMember(filetime.getStruct()); //currentTime
+        statusStruct.addMember(filetime.getStruct()); //lastUpdate
+        statusStruct.addMember("Short"); //serverState
+        statusStruct.addMember("Integer"); //groupCount
+        statusStruct.addMember("Integer"); //bandwidth
+        statusStruct.addMember("Short"); //majorVersion
+        statusStruct.addMember("Short"); //minorVersion
+        statusStruct.addMember("Short"); //buildNumber
+        statusStruct.addMember("Short"); //reserved
+        statusStruct.addMember(new dcom.Pointer(new dcom.ComString(dcom.Flags.FLAG_REPRESENTATION_STRING_LPWSTR))); //vendorInfo
+
+        let statusStructPointer = new dcom.Pointer(statusStruct);
+
+        let callObject = new dcom.CallBuilder(true);
+        callObject.setOpnum(3);
+        callObject.addOutParamAsObject(statusStructPointer, dcom.Flags.FLAG_NULL);
+
+        let result = await this._comObj.call(callObject);
+
+        let resStruct = result[0].getReferent();
+
+        return {
+            startTime: filetime.fromStruct(resStruct.getMember(0)).getDate(),
+            currentTime: filetime.fromStruct(resStruct.getMember(1)).getDate(),
+            lastUpdateTime: filetime.fromStruct(resStruct.getMember(2)).getDate(),
+            serverState: resStruct.getMember(3),
+            groupCount: resStruct.getMember(4),
+            bandWidth: resStruct.getMember(5),
+            majorVersion: resStruct.getMember(6),
+            minorVersion: resStruct.getMember(7),
+            buildNumber: resStruct.getMember(8),
+            reserved: resStruct.getMember(9),
+            vendorInfo: resStruct.getMember(10).getReferent().getString()
+        };
     }
 
     /**
      * 
-     * @param {*} handle 
+     * @param {OPCGroupStateManager|number} handle
      * @param {boolean} [force=false]
      * @returns {Promise<?>}
      * @opNum 4
@@ -118,17 +235,46 @@ class OPCServer {
     async removeGroup(handle, force) {
         if (!this._comObj) throw new Error("Not initialized");
 
+        if (handle instanceof OPCGroupStateManager) {
+            handle = groupCache.get(handle);
+        }
+        if (typeof handle != 'number') {
+            throw new Error("Cannot find handle for the provided group");
+        }
+
+        let callObject = new dcom.CallBuilder(true);
+        callObject.setOpnum(4);
+
+        callObject.addInParamAsInt(handle, dcom.Flags.FLAG_NULL);
+        callObject.addInParamAsInt(force ? 1 : 0, dcom.Flags.FLAG_NULL);
+
+        await this._comObj.call(callObject);
     }
 
     /**
      * 
-     * @param {number} scope
-     * @returns {Promise<?>}
+     * @param {number} scope an OPC scope
+     * @returns {Promise<string[]>}
      * @opNum 5
      */
     async getGroups(scope) {
         if (!this._comObj) throw new Error("Not initialized");
 
+        let callObject = new dcom.CallBuilder(true);
+        callObject.setOpnum(5);
+
+        callObject.addInParamAsShort(scope, dcom.Flags.FLAG_NULL);
+        callObject.addInParamAsUUID(constants.iid.IEnumString_IID, dcom.Flags.FLAG_NULL);
+        callObject.addOutParamAsType("ComObject", dcom.Flags.FLAG_NULL);
+
+        let result = await this._comObj.call(callObject);
+
+        let enumStr = new EnumString();
+        await enumStr.init(result[0]);
+        let res = await enumStr.asArray();
+        await enumStr.end();
+        
+        return res;
     }
 
     // ------
@@ -141,7 +287,7 @@ class OPCServer {
 
         if (!this._opcCommon) {
             let opcCommon = new OPCCommon(); //TODO pass comObj handle
-            await this._opcCommon.init();
+            await this._opcCommon.init(this._comObj);
             this._opcCommon = opcCommon;
         }
         return this._opcCommon;
@@ -155,7 +301,7 @@ class OPCServer {
 
         if (!this._opcBrowser) {
             let opcBrowser = new OPCBrowser(); //TODO pass comObj handle
-            await this._opcBrowser.init();
+            await this._opcBrowser.init(this._comObj);
             this._opcBrowser = opcBrowser;
         }
         return this._opcBrowser;
@@ -169,7 +315,7 @@ class OPCServer {
 
         if (!this._opcItemIO) {
             let opcItemIO = new OPCItemIO(); //TODO pass comObj handle
-            await this._opcItemIO.init();
+            await this._opcItemIO.init(this._comObj);
             this._opcItemIO = opcItemIO;
         }
         return this._opcItemIO;
@@ -183,7 +329,7 @@ class OPCServer {
 
         if (!this._opcItemProperties) {
             let opcItemProperties = new OPCItemProperties(); //TODO pass comObj handle
-            await this._opcItemProperties.init();
+            await this._opcItemProperties.init(this._comObj);
             this._opcItemProperties = opcItemProperties;
         }
         return this._opcItemProperties;
