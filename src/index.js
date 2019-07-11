79eece2653f78c9c714c8e91fbe61514cf3ebfff
Guilherme Francescon <guilherme.francescon@netsmarttech.com>
2019-07-11 19:23:48 -0300

    Export more objects

--- src/index.js
+++ src/index.js
@@ -4,8 +4,40 @@
   GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 */
 
-const constants = require('./constants.js');
+const constants = require('./constants');
+const OPCAsyncIO = require('./opcAsyncIO');
+const OPCBrowser = require('./opcBrowser');
+const OPCCommon = require('./opcCommon');
+const OPCGroupStateManager = require('./opcGroupStateManager');
+const OPCItemIO = require('./opcItemIO');
+const OPCItemManager = require('./opcItemManager');
+const OPCItemProperties = require('./opcItemProperties');
+const OPCServer = require('./opcServer');
+const OPCSyncIO = require('./opcSyncIO');
+
+/**
+ * 
+ * @param {string} domain 
+ * @param {string} user 
+ * @param {string} pass 
+ * @param {string} clsid 
+ * @returns {Promise<OPCServer>}
+ */
+async function createServer(domain, user, pass, clsid) {
+
+}
 
 module.exports = {
-    constants,
+  constants,
+  OPCAsyncIO,
+  OPCBrowser,
+  OPCCommon,
+  OPCGroupStateManager,
+  OPCItemIO,
+  OPCItemManager,
+  OPCItemProperties,
+  OPCServer,
+  OPCSyncIO,
+
+  createServer
 }
